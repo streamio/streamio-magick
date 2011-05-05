0651698e19c15f9d376eefe26088b64bab4b7b84
David Backeus <duztdruid@gmail.com>
2011-05-05 11:50:23 +0200

    Use Bundler to manage development / test environment. Updated to rspec version 2.5.0.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,9 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'streamio-magick'
-require 'spec'
-require 'spec/autorun'
+require 'bundler'
+Bundler.require
+
 require 'fileutils'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
 end
 
 def fixture_path
